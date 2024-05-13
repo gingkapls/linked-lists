ecdf297889798ff4c38178ecdc3b0bd746d3e257
gin <73906888+gingkapls@users.noreply.github.com>
2024-05-13 20:14:02 +0530

    LinkedList: add append, prepend, print, size, head and tail

---
+++ LinkedList.js
@@ -0,0 +1,65 @@
+const Node = require("./Node.js");
+
+const LinkedList = class {
+  #head;
+  #size;
+
+  constructor() {
+    this.#head = null;
+    this.#size = 0;
+  }
+
+  append = (value) => {
+    // List has no nodes
+    if (this.#head === null) {
+      this.#head = new Node({ value });
+      this.#size += 1;
+      return this;
+    }
+
+    let h = this.#head;
+    while (h?.next != null) {
+      h = h.next;
+    }
+    h.next = new Node({ value });
+
+    this.#size += 1;
+    return this;
+  };
+
+  prepend = (value) => {
+    const next = this.#head;
+    this.#head = new Node({ value });
+    this.#head.next = next;
+    this.#size += 1;
+    return this;
+  };
+
+  print = () => {
+    let h = this.#head;
+    let str = "";
+    while (h != null) {
+      str += `( ${h.value} ) -> `;
+      h = h.next;
+    }
+    return `${str}null`;
+  };
+
+  get size() {
+    return this.#size;
+  }
+    
+  get head() {
+    return this.#head;
+  }
+
+  get tail() {
+    let h = this.#head;
+    while (h.next != null) {
+        h = h.next;
+    }
+    return h;
+  }
+};
+
+module.exports = LinkedList;
